fcd7febe41b386ea0b03ce31f1975f2ac35d9ea6
radouane <radouane.eladlani@gmail.com>
2024-06-04 14:49:03 +0200

    creer un système de mock

--- mon-app-dashboard/src/utils/useFetch.js
+++ mon-app-dashboard/src/utils/useFetch.js
@@ -1,5 +1,6 @@
 import { useState, useEffect, useCallback } from 'react';
-
+import { USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS, USER_PERFORMANCE } from '../mock/data';
+const mode = "dev";
 const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -7,18 +8,38 @@ const useFetch = (url) => {
 
   const fetchData = useCallback(async () => {
     setLoading(true);
-
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Error: ${response.statusText}`);
+    if (mode === "dev") {
+      const module = url.substring(url.lastIndexOf("/") + 1);
+      switch (module) {
+        case "activity":
+          setData({data: USER_ACTIVITY});
+          break;
+        case "average-sessions":
+          setData({data: USER_AVERAGE_SESSIONS}); 
+          break;
+        case "performance":
+          setData({data:USER_PERFORMANCE});
+          break;
+        default:
+          setData({data:USER_MAIN_DATA});
+          break;
       }
-      const result = await response.json();
-      setData(result);
-    } catch (err) {
-      setError(err.message);
-    } finally {
       setLoading(false);
+    } else {
+
+
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Error: ${response.statusText}`);
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
     }
   }, [url]);
 
